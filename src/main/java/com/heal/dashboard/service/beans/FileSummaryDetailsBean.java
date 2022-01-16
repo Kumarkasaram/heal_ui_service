5514f61c0ca3b77e2e96b2a2fca6d28fcce72e28
tech_dip <imdip95@gmail.com>
2022-01-16 07:57:48 +0530

    "file upload service"

--- src/main/java/com/heal/dashboard/service/beans/FileSummaryDetailsBean.java
+++ src/main/java/com/heal/dashboard/service/beans/FileSummaryDetailsBean.java
@@ -1,2 +1,12 @@
-package com.heal.dashboard.service.beans;public class FileSummaryDetailsBean {
+package com.heal.dashboard.service.beans;
+
+import lombok.Data;
+
+@Data
+public class FileSummaryDetailsBean {
+    private int fileProcessedId;
+    private String key;
+    private String value;
+    private int accountId;
+    private int isDebugLogs;
 }
